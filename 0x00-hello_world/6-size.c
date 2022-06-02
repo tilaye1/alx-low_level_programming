b2cca7d6f80ba32f0e86e45eb320e57ff110d91e
tilaye1 <til_mer@yahoo.com>
2022-06-02 13:16:36 -0700

    first time on c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-int main(viod){
+int main(void)
+{
 	printf("size of a char: %zu byte(s)\n", sizeof(char));
-	printf("size of a int: %zu byte(S)\n", sizeof(int));
-	printf("size of a long int: %zu byte(S)\n", sizeof(long int));
-	printf("size of a long long int: %zu byte(S)\n", sizeof(long long int));
-	printf("size of a float: %zu byte(S)\n", sizeof(float));
+	printf("size of a int: %zu byte(s)\n", sizeof(int));
+	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %zu byte(s)\n", sizeof(float));
 	return(0);
 }
