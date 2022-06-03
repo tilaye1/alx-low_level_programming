5cde769f308189445a068dbd290ae9f422564752
tilaye1 <til_mer@yahoo.com>
2022-06-03 09:39:36 -0700

    still on alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if ((alpha != 'q') && (alpha != 'e'))
+		{
+			putchar(alpha);
+		}
+
+		alpha++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
