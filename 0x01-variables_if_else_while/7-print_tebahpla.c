83c2bca58975552b129ca3a281f20925f5437eae
tilaye1 <til_mer@yahoo.com>
2022-06-03 13:38:38 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- entry point for printing numbers
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+
+	return (0);
+}
