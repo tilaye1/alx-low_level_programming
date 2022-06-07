1d592eab241da7e577f82cc08fa00dd8dd0726b2
tilaye1 <til_mer@yahoo.com>
2022-06-07 13:53:10 -0700

    nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,11 +13,10 @@ void print_alphabet_x10(void)
 	for (counter = 0; counter <= 10; counter++)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
-				{
-					_putchar(alpha);
-				}
-				_putchar('\n');
-			
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');	
 	}
 	
 }
