0d7d634130128f44c4bed6e7222608ce1bc98746
tilaye1 <til_mer@yahoo.com>
2022-06-07 12:25:54 -0700

    print char

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_H
+#define _MAINN_
+
+/**
+ * _putchar: write to stdout
+ * @c: character to print
+ *
+ *
+ * Return : 1 on success
+ */
+int _putchar(char c)
+
+
+
+
+
+
+
+
+#endif 
