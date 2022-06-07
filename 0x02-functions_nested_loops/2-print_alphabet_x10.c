614ae17afbde296a448a59ce697a6b05358b8877
tilaye1 <til_mer@yahoo.com>
2022-06-07 13:41:24 -0700

    alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- Entry print to Print lower  case alphabet
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+
+	int counter;
+
+	for (counter = 0; counter < = 10; counter++)
+	{
+		for (alpha = 'a'; alpha = 'z'; alpha++)
+				{
+					_putchar(alpha);
+				}
+				_putchar('\n')
+			
+	}
+	
+}
