95dece852b6973acdc5524e7d4e13ac70b8b22f6
tilaye1 <til_mer@yahoo.com>
2022-06-08 13:33:22 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - last digit find
+ * @n: chekced number
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+
+	n * = -1;
+	}
+	_putchar('0' + (n % 10));
+	return(n % 10);
+}
