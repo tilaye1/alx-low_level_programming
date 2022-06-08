096e9022d5104874ccadef49380cef6fef9740d9
tilaye1 <til_mer@yahoo.com>
2022-06-08 13:08:59 -0700

    nested loop exmalpe

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - sign number chck
+ * @n: check number
+ * Return: 1 if greater and 0 will equal and else -1 from 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return ('-1');
+	}
+}
