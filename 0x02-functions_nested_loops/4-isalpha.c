3af57368c1739e981fb02df466e2cde80e1848dd
tilaye1 <til_mer@yahoo.com>
2022-06-08 12:57:22 -0700

    nested loop example

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+nclude "main.h"
+/**
+ * _isalpha - checks wether it is character
+ * @c: character to check
+ * Return: 1 if lower or upper, else 0
+ */
+int _isalpha(inc c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
