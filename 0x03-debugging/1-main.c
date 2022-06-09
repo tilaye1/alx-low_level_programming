8da5a72f2389704cf5c4ffab603652718c661887
tilaye1 <til_mer@yahoo.com>
2022-06-09 16:25:19 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	printf("infinite loop coming :(\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
@@ -19,6 +19,6 @@ int main(void)
 		putchar(i);
 	}
 	*/
-	printf("infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
