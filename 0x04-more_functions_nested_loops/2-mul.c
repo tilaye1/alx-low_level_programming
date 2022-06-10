4f19b58118f7deaca12ce94ee624314375cd6421
tilaye1 <til_mer@yahoo.com>
2022-06-10 13:24:06 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiply two number
+ * @a: number
+ * @b: number
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ * Return: 0 on succes
+ */
+void print_numbers(void)
+{
+	char i ;
+
+	for (i = '0'; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n')
