2f83ec829ca227661f4ec9de161aea96b4cda167
tilaye1 <til_mer@yahoo.com>
2022-06-10 13:02:41 -0700

    nested loop

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+/**
+ * _putchar: function
+ * @c: character checked
+ * Return: return 1 on success
+ */
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void print_line(void);
+
+int _putchar(char c);
+
 /**
  * print_number - print an integer
  * @n: number to be printed
