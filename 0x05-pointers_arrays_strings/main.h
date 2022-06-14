f3d6eb844c343a3f85f2398cc34e6c6d9d7c559f
tilaye1 <til_mer@yahoo.com>
2022-06-13 23:05:42 -0700

    c pointer

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,47 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - print character on stdout
+ * @n: char 
+ */
+int _putchar(char c);
+
+/**
+ * reset_to_98 - reset pointed variable value
+ * @n: pointer
+ */
+void reset_to_98(int *n);
+
+/**
+ *swap_int - swap two integer
+ *@a: integer
+ *@b:integer
+ */
+void swap_int(int *a, int *b);
+
+/**
+ * _atoi - convert string to int
+ * @s: string for conversion
+ * Return: integer
+ */
+
+/**
+ * main - return the lenght if string
+ * @s: string
+ */
+int _strlen(char *s);
+
+/**
+ * _puts - puts string on stdout
+ * @str: string
+ */
+
+void _puts(char *str);
+
+/**
+ * print_rev - print string in reverse
+ * @s: string
+ */
+void print_rev(char *s);
+int _atoi(char *s);
