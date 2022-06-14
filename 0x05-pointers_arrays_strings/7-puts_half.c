e6863743a93294b3e65bb729235ef46d423812cb
tilaye1 <til_mer@yahoo.com>
2022-06-14 00:52:00 -0700

    print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strlen - check string length
+ * @s: string
+ * Return: return value
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * puts_half - print half of string
+ * @str: string
+ */
+
+void half_string(char *str)
+{
+	int idx;
+	int len = _strlen(str);
+
+	if (len % 2 != 0)
+		idx = (len / 2) + 1;
+	else
+		idx  = len / 2;
+	while (idx < len)
+	{
+		_putchar(*(str + idx));
+		idx++;
+	}
+	_putchar('\n');
+}
+
