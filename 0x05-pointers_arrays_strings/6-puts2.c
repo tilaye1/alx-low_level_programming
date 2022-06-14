5f0b08d4c5dea451dead66fd4f33bf453e753738
tilaye1 <til_mer@yahoo.com>
2022-06-14 00:36:44 -0700

    print second string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strlen - check string length
+ * @s: string
+ * Return: return value
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * puts2 - print second character
+ * @str: string to print
+ */
+void puts2(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
+		_putschar(str[i]);
+
+	_putschar('\n');
+}
