207cd2f5666c4ab21b9ba16909aa9b942849be50
tilaye1 <til_mer@yahoo.com>
2022-06-14 01:33:35 -0700

    string copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,5 @@
 #include "main.h"
-
-/**
+/** 
  * _strcpy - copy a string with null byte
  * @dest: a place where to put
  * @src: a string to be copied
@@ -8,11 +7,24 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-	while (*src)
-	{
-		*dest = *src;
-		src++;
-		dest++;
-	}
+	int i;
+	for(i = 0; i <= _strlen(src); i++)
+		dest[i] = src[i];
 	return (dest);
+}
+/**
+ * _strlen - check string length
+ * @s: string
+ * Return: return value
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
 }
